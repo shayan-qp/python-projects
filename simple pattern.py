fcd6acf6775310ee6292ab0d3b0bac2b7392cd26
shayan-qp <shayan121285a@gmail.com>
2024-06-20 16:05:54 +0330

    simple-pattern.py

--- simple pattern.py
+++ simple pattern.py
@@ -1,8 +1,16 @@
 x="*"
 
-for i in range(5):
+for a in range(5):
     print(x)
     y=len(x)
     x="*"
     z=y+1
     x=x*z
+
+# or
+
+s="*"
+for i in range(1,6):
+    for j in range(i):
+        print(s,end="")
+    print(end="\n")
